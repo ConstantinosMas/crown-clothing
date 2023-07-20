1c13e6a587c6b1fdfb68b39a18e8d2a320596c14
ConstantinosMas <mastrapasconstantinos@yahoo.gr>
2023-07-20 13:35:11 +0300

    add logo animation to home page

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,11 +1,19 @@
 import Directory from '../../components/directory/directory.component';
+import leftArrow from '../../assets/left-arrow.png';
+import rightArrow from '../../assets/right-arrow.png';
+import crwn from '../../assets/crwn.png';
+import './home.styles.scss';
 
 
 const Home = () => {
 
   return (
-    <div>
+    <div className='container'>
         <Directory />
+
+        <img id="slideLeft" src={leftArrow} />
+        <img id="slideRight" src={rightArrow} />
+        <img id='circle-logo' className='fade-in' src={crwn} />
     </div>
   );
 }
